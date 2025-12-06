8b4f6b5bfd56259bd8c46feae8870abdda0a1b69
bartlettjude <bartlettjude@gmail.com>
2025-12-06 15:18:41 -0500

    Force inquiry buttons to use white text

--- app/page.tsx
+++ app/page.tsx
@@ -118,7 +118,12 @@ export default function Home() {
               Interested in working together? Share a few details below.
             </p>
           </div>
-          <Button onClick={() => setInquiryOpen(true)} href="#" variant="primary" className="text-white">
+          <Button
+            onClick={() => setInquiryOpen(true)}
+            href="#"
+            variant="primary"
+            className="!text-white"
+          >
             Begin Inquiry
           </Button>
         </div>
